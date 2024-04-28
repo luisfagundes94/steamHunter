20604b67dccf639c71ed3940e4f420e6101f4085
luisfagundes94 <luisfelipecf94@gmail.com>
2024-04-28 12:32:55 -0300

    removing comments

--- core/domain/src/main/java/com/luisfagundes/domain/repository/UserDataRepository.kt
+++ core/domain/src/main/java/com/luisfagundes/domain/repository/UserDataRepository.kt
@@ -6,14 +6,7 @@ import kotlinx.coroutines.flow.Flow
 
 interface UserDataRepository {
 
-    /**
-     * Stream of [UserData]
-     */
     val userData: Flow<UserData>
 
-    /**
-     * Sets the desired dark theme config.
-     */
     suspend fun setDarkThemeConfig(darkThemeConfig: DarkThemeConfig)
-
 }
