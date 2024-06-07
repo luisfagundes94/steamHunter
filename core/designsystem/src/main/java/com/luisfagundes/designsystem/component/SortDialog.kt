e46e1d1dfcd6ff0130a97d4c9d0056299c90092e
Luis Felipe Fagundes <luisfelipecf94@gmail.com>
2024-06-07 17:05:07 -0300

    implementing sort dialog component

---
+++ core/designsystem/src/main/java/com/luisfagundes/designsystem/component/SortDialog.kt
@@ -0,0 +1,78 @@
+package com.luisfagundes.designsystem.component
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+
+@Composable
+fun SortDialog(
+    modifier: Modifier = Modifier,
+    onDismissRequest: () -> Unit,
+    onSortOptionSelected: (String) -> Unit
+) {
+    var selectedOption by remember { mutableStateOf("Name") }
+
+    Dialog(onDismissRequest = onDismissRequest) {
+        Surface(
+            shape = MaterialTheme.shapes.medium,
+            color = MaterialTheme.colorScheme.background,
+            contentColor = contentColorFor(MaterialTheme.colorScheme.background)
+        ) {
+            Column(
+                modifier = modifier
+            ){
+                Text(
+                    text = "Sort by",
+                    style = MaterialTheme.typography.titleMedium,
+                    modifier = Modifier.padding(bottom = 16.dp)
+                )
+                RadioButtonGroup(
+                    options = listOf("Name", "Achievement Percentage", "Date"),
+                    selectedOption = selectedOption,
+                    onOptionSelected = { selectedOption = it }
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+                Button(
+                    onClick = { onSortOptionSelected(selectedOption) },
+                    modifier = Modifier.align(Alignment.End)
+                ) {
+                    Text("OK")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun RadioButtonGroup(
+    options: List<String>,
+    selectedOption: String,
+    onOptionSelected: (String) -> Unit
+) {
+    Column {
+        options.forEach { option ->
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.padding(vertical = 4.dp)
+            ) {
+                RadioButton(
+                    selected = option == selectedOption,
+                    onClick = { onOptionSelected(option) }
+                )
+                Text(
+                    text = option,
+                    style = MaterialTheme.typography.titleSmall,
+                    modifier = Modifier.padding(start = 8.dp)
+                )
+            }
+        }
+    }
+}
