79c596f133bd9a5eb421d1a503d30b2215aa0ab0
Luis Felipe Fagundes <luisfelipecf94@gmail.com>
2024-06-19 19:34:23 -0300

    adding fake game model

--- core/testing/src/main/java/com/luisfagundes/testing/model/GamesTestModel.kt
+++ core/testing/src/main/java/com/luisfagundes/testing/model/GamesTestModel.kt
@@ -3,19 +3,19 @@ package com.luisfagundes.testing.model
 import com.luisfagundes.model.Game
 
 val games = listOf(
-    com.luisfagundes.model.Game(
+    Game(
         appId = 0,
         name = "Hunt: Showdown",
         achievementsUnlocked = 20,
         achievementsTotal = 40
     ),
-    com.luisfagundes.model.Game(
+    Game(
         appId = 1,
         name = "Horizon: Zero Dawn",
         achievementsUnlocked = 20,
         achievementsTotal = 40
     ),
-    com.luisfagundes.model.Game(
+    Game(
         appId = 2,
         name = "Diablo IV",
         achievementsUnlocked = 20,
