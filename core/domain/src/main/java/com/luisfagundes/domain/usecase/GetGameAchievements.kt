db6e39db4102767c80c6f1f5cd7d49888d7d87c3
Luis Felipe Fagundes <luisfelipecf94@gmail.com>
2024-06-21 11:33:35 -0300

    replacing log with timber

--- core/domain/src/main/java/com/luisfagundes/domain/usecase/GetGameAchievements.kt
+++ core/domain/src/main/java/com/luisfagundes/domain/usecase/GetGameAchievements.kt
@@ -1,26 +1,21 @@
 package com.luisfagundes.domain.usecase
 
-import android.util.Log
-import com.luisfagundes.model.Achievement
 import com.luisfagundes.domain.repository.SteamRepository
 import com.luisfagundes.domain.repository.UserDataRepository
 import com.luisfagundes.result.Result
-import com.luisfagundes.result.getResultOrThrow
 import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.runBlocking
+import timber.log.Timber
 import javax.inject.Inject
 
 class GetGameAchievements @Inject constructor(
     private val steamRepository: SteamRepository,
     private val userRepository: UserDataRepository
 ) {
-    suspend operator fun invoke(appId: Int): Result<List<Achievement>?> {
-        return try {
-            val steamId = userRepository.userData.first().steamId
-            steamRepository.getAchievements(steamId, appId)
-        } catch (e: Exception) {
-            Log.e("GetGameAchievements", e.stackTraceToString())
-            Result.Error(e)
-        }
+    suspend operator fun invoke(appId: Int) = try {
+        val steamId = userRepository.userData.first().steamId
+        steamRepository.getAchievements(steamId, appId)
+    } catch (exception: Exception) {
+        Timber.tag("GetGameAchievements").e(exception.stackTraceToString())
+        Result.Error(exception)
     }
 }
