31cfe7126305802df2dd3367f33ebcf56f1e3379
Luis Felipe Fagundes <luisfelipecf94@gmail.com>
2024-06-21 11:34:01 -0300

    implementing init logic for timber logging

--- app/src/main/java/com/luisfagundes/steamhunter/SteamHunterApplication.kt
+++ app/src/main/java/com/luisfagundes/steamhunter/SteamHunterApplication.kt
@@ -3,6 +3,19 @@ package com.luisfagundes.steamhunter
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
-class SteamHunterApplication : Application()
+class SteamHunterApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        initTimber()
+    }
+
+    private fun initTimber() {
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+}
