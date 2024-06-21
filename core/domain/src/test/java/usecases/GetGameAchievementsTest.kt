0a71aa05cb0c483b9b55715f63a9ca8c7922c037
Luis Felipe Fagundes <luisfelipecf94@gmail.com>
2024-06-21 11:33:42 -0300

    writing unit tests for usecase

---
+++ core/domain/src/test/java/usecases/GetGameAchievementsTest.kt
@@ -0,0 +1,75 @@
+package usecases
+
+import com.luisfagundes.domain.repository.SteamRepository
+import com.luisfagundes.domain.repository.UserDataRepository
+import com.luisfagundes.domain.usecase.GetGameAchievements
+import com.luisfagundes.result.Result
+import com.luisfagundes.testing.model.achievements
+import com.luisfagundes.testing.model.userData
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.mockk
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Test
+import kotlin.test.assertEquals
+
+@ExperimentalCoroutinesApi
+class GetGameAchievementsTest {
+
+    private val steamRepository: SteamRepository = mockk()
+    private val userRepository: UserDataRepository = mockk()
+    private lateinit var getGameAchievements: GetGameAchievements
+
+    @Before
+    fun setUp() {
+        getGameAchievements = GetGameAchievements(steamRepository, userRepository)
+    }
+
+    @Test
+    fun `invoke should return achievements list when repository call is successful`() = runTest {
+        val steamId = "123"
+        val appId = 67890
+
+        coEvery { userRepository.userData } returns flowOf(userData)
+        coEvery { steamRepository.getAchievements(steamId, appId) } returns Result.Success(achievements)
+
+        val result = getGameAchievements(appId)
+
+        assertEquals(result, Result.Success(achievements))
+        coVerify { userRepository.userData }
+        coVerify { steamRepository.getAchievements(steamId, appId) }
+    }
+
+    @Test
+    fun `invoke should return error when repository call fails`() = runTest {
+        val steamId = "123"
+        val appId = 67890
+        val exception = Exception("Error")
+
+        coEvery { userRepository.userData } returns flowOf(userData)
+        coEvery { steamRepository.getAchievements(steamId, appId) } returns Result.Error(exception)
+
+        val result = getGameAchievements(appId)
+
+        assertEquals(result, Result.Error(exception))
+        coVerify { userRepository.userData }
+        coVerify { steamRepository.getAchievements(steamId, appId) }
+    }
+
+    @Test
+    fun `invoke should return error when getting user data fails`() = runTest {
+        val appId = 67890
+        val exception = Exception("User Data Error")
+
+        coEvery { userRepository.userData } throws exception
+
+        val result = getGameAchievements(appId)
+
+        assertEquals(result, Result.Error(exception))
+        coVerify { userRepository.userData }
+        coVerify(exactly = 0) { steamRepository.getAchievements(any(), any()) }
+    }
+}
