b99b8ed486d0f5d63170af2d5fb39cc241538705
Luis Felipe Fagundes <luisfelipecf94@gmail.com>
2024-06-24 10:58:39 -0300

    removing unused imports

--- core/common/src/main/java/com/luisfagundes/result/SafeAsyncRequest.kt
+++ core/common/src/main/java/com/luisfagundes/result/SafeAsyncRequest.kt
@@ -1,6 +1,5 @@
 package com.luisfagundes.result
 
-import android.util.Log
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
